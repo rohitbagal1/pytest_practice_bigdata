2ded7a30bf7bcd22c12dd3d72c8a468d1090118c
rohitbagal1 <90562891+rohitbagal1@users.noreply.github.com>
2024-09-16 17:27:00 +0530

    Update data.py

--- test_data/data.py
+++ test_data/data.py
@@ -1,8 +1,4 @@
 
-#output_path_csv = "/FileStore/test_df/my_table"
-
-# Test Data:
-path = "/FileStore/test_df/my_table"
 columns_to_add = ['city']
 columns_to_drop = ['city']
 column_to_capitalize = ['Address']
