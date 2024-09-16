1eaea40f5df68aea8355ba27672165bc9cd5c470
rohitbagal1 <90562891+rohitbagal1@users.noreply.github.com>
2024-09-16 17:23:32 +0530

    Update test_one.py

--- test_cases/test_one.py
+++ test_cases/test_one.py
@@ -3,7 +3,6 @@ from test_data import data
 from transformation.transformation_class import TransFormClass
 class TestMathOperations(TransFormClass):
     def test_addition(self):
-        
          df=self.execute_transformations(data.columns_to_add, data.columns_to_drop, data.column_to_capitalize)
          assert df.collect()!=df.collect(),"having hava error"
 
